febf05c4893617e85b1ab986f8aacc5e08d6d8b1
dudematthew <mmoczydlowski4@gmail.com>
2024-01-16 13:09:36 +0100

    Initial commit

---
+++ push-prod.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# merge branch
+
+git pull
+
+git push
+
+git checkout production
+
+git pull
+
+git merge main
+
+# stash any uncommitted changes
+git stash
+
+# bump version based on the first argument to the script
+npm version $VERSION -m "Upgrade to %s"
+
+# apply the stash
+git stash pop
+
+git push
+
+git checkout main
+
+git merge production
+
+git push
+
+# if you want to keep the terminal open
+# /bin/bash
